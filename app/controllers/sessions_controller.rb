4583586b7c4ff33ec0312c2e61b8f26b17a904e2
Omar Labana <omarlabana@outlook.com>
2021-09-02 21:19:43 +0200

    Edit session store

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
+  include CurrentUserConcern
 
   def create
     user = User
-            .find_by(username: params["user"]["username"])
+            .find_by(email: params["user"]["email"])
             .try(:authenticate, params["user"]["password"])
-    puts user
+
     if user
       session[:user_id] = user.id
       render json: {
@@ -17,4 +18,21 @@ class SessionsController < ApplicationController
     end
   end
 
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    else
+      render json: {
+        logged_in: false
+      }
+    end
+  end
+
+  def logout
+    reset_session
+    render json: { status: 200, logged_out: true }
+  end
 end
