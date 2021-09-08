be3b83e65cf5f16026592d4d9df00f79309b0fa9
Omar Labana <omarlabana@outlook.com>
2021-09-08 14:21:22 +0200

    Add basic db seedings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Jacket.create(name: 'Yellow Jacket', price:'15$',description:'Very cool jacet', image_url:'https://www.petrolindustries.com/media/image/6e/2a/5d/B-3000-JAC109-1034-06_1400x2000.jpg')
+Jacket.create(name: 'Blue Jacket', price:'15$',description:'Very cool jacet', image_url:'https://www.hummel.dk/dw/image/v2/BDWL_PRD/on/demandware.static/-/Sites-hummel-master-catalog/default/dw15754295/images/packshot/206687-7045.png?sw=514&sh=685&sm=fit&q=80')
+Jacket.create(name: 'Gray Jacket', price:'15$',description:'Very cool jacet', image_url:'https://redbridgejeans.de/media/image/product/65475/lg/m6075_red-bridge-mens-parka-jacket-coat-winter-jacket-rb-patches-m6075_18.jpg')
