43b867b73a23b7ab25297c856b29668f8bc3510b
Omar Labana <omarlabana@outlook.com>
2021-09-08 14:19:44 +0200

    Add jackets controller

---
+++ app/helpers/jackets_helper.rb
@@ -0,0 +1,2 @@
+module JacketsHelper
+end
---
+++ test/controllers/jackets_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class JacketsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
