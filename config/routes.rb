a41335496655d74ee9d62e6cba49117b4d41fba7
Omar Labana <omarlabana@outlook.com>
2021-09-08 14:21:11 +0200

    Add routing for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :registrations, only: [:create]
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
+
+
+  resources :jackets
+  resources :whishlists
 end
