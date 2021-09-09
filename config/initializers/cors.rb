1d9c2bebe042b53b7b63d98be69df7e105828409
Omar Labana <omarlabana@outlook.com>
2021-09-10 01:28:15 +0200

    Heroku config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,9 @@
-# frozen_string_literal: true
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
-    resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
-  end
+    origins '*'
 
-  allow do
-    # to be changed
-    origins 'https://fuyu-jackets.herokuapp.com/'
-    resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
