a33e5f182506b0126d137ea6ebe17ac6384081f3
Omar Labana <omarlabana@outlook.com>
2021-09-10 01:05:38 +0200

    Heroku config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
   allow do
     # to be changed
-    origins 'https://your-production-app-here.com'
+    origins 'https://fuyu-jackets.herokuapp.com/'
     resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
   end
 end
